9c92d9660276cc9d451df3e650b3fd9cce083361
Weffe <rogelio_negrete@live.com>
2019-03-27 22:08:10 -0700

    feat: add a Map for events

--- src/tests/infra/SocketIOServer.ts
+++ src/tests/infra/SocketIOServer.ts
@@ -1,14 +1,21 @@
 import http from 'http';
 import IOServer from 'socket.io';
 
+type RegisteredEventValueType = {
+    eventName: string;
+    callback: Function;
+}
+
 export class SocketIOServer {
     public readonly port = 4444;
     private httpServer: http.Server;
     private sioServer: SocketIO.Server;
+    private registeredEvents: Map<Symbol, RegisteredEventValueType>;
 
     public constructor() {
         this.httpServer = http.createServer();
         this.sioServer = IOServer(this.httpServer);
+        this.registeredEvents = new Map();
     }
 
     private onServerStart = () => {
@@ -17,15 +24,32 @@ export class SocketIOServer {
             socket.on('disconnect', () => {
                 console.log('A user has disconnected...');
             });
+
+            const events = this.registeredEvents.values();
+
+            for (let event of events) {
+                socket.on(event.eventName, (...data: any[]) => {
+                    event.callback(...data);
+                });
+            }
         });
+    }
+
+    public registerEvent = (eventName: string, callback: Function): Symbol => {
+        const id = Symbol(eventName);
+        this.registeredEvents.set(id, { eventName, callback });
+
+        return id;
+    }
+
+    public unregisterEvent = (id: Symbol) => {
+        this.registeredEvents.delete(id);
     }
 
     public start = () => {
         this.onServerStart();
 
-        this.httpServer.listen(this.port, () => {
-            console.log(`Starting Socket IO Server locally on port ${this.port}`);
-        });
+        this.httpServer.listen(this.port);
     }
 
     public stop = () => {
